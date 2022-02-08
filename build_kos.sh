9d7b4c8ad1ec4a90c87f7a6eae33d357cf413a1f
retrohead <retro_head@hotmail.com>
2022-02-08 21:55:07 +0000

    updated kos script

--- build_kos.sh
+++ build_kos.sh
@@ -1,105 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# msys2 installer for Windows: http://repo.msys2.org/distrib/i686/msys2-i686-20160205.exe
-
-# Stop script on error
-set -e
-
-is_root_user()   { [ $(id -u) -eq 0 ]; }
-program_exists() { command -v "$1" >/dev/null 2>&1; }
-
-is_root_user && { echo "Please don't run this script as root."; exit 1; }
-
-if program_exists getconf; then
-	CORE_COUNT="$(getconf _NPROCESSORS_ONLN)"
-else
-	CORE_COUNT=2
-fi
-
-# Force mingw32 shell on MSYS 32 bit, disallow MSYS 64 bit (untested)
-HAVE_MINGW32=0
-case "$(uname)" in
-	"MINGW64"*|"MSYS"*)
-		echo "Please use c:\msys32\mingw32_shell.bat"
-		exit 1
-		;;
-	"MINGW32"*)
-		echo "MingW32 detected."
-		HAVE_MINGW32=1
-		;;
-esac
-
-if [ $HAVE_MINGW32 -eq 0 ] && ! program_exists sudo; then
-	echo "Please install sudo."
-	exit 1
-fi
-
-# Install requirements on msys2
-if [ $HAVE_MINGW32 -eq 1 ]; then
-	# Don't upgrade packages here because it requires restarting the shell and loses Windows XP support (bash etc. will miss functions in the Windows XP kernel)
-	pacman -Sy --needed --noconfirm \
-		mingw-w64-i686-binutils \
-		mingw-w64-i686-gcc \
-		mingw-w64-i686-pkg-config \
-		mingw-w64-i686-libpng \
-		mingw-w64-i686-libjpeg-turbo \
-		diffutils git make subversion patch python tar texinfo wget
-# Install requirements using OS X homebrew
-elif program_exists brew; then
-	brew install libjpeg libpng
-	brew link libjpeg libpng
-# Install required programs on debian or derived distributions
-elif program_exists apt-get; then
-	sudo apt-get -y install binutils bzip2 curl gcc pkg-config libpng-dev libjpeg-dev diffutils git make subversion patch python tar texinfo wget
-elif program_exists yum && program_exists sudo; then
-	sudo yum -y install binutils bzip2 curl gcc pkgconfig libpng-devel libjpeg-devel diffutils git make subversion patch python tar texinfo wget
-# Install required programs on gentoo or derived distributions
-elif program_exists emerge && program_exists sudo; then
-	sudo emerge -an sys-devel/binutils app-arch/bzip2 net-misc/curl sys-devel/gcc dev-util/pkgconfig media-libs/libpng media-libs/libjpeg-turbo sys-apps/diffutils dev-vcs/git sys-devel/make dev-vcs/subversion sys-devel/patch dev-lang/python app-arch/tar sys-apps/texinfo net-misc/wget
-# Catch-all, installs requirements when no package manager is available
-else
-	echo "Checking for zlib, libjpeg and libpng.."
-	[ -d /usr/local/src ] || sudo mkdir -p /usr/local/src
-
-	if ([ ! -f /usr/include/zlib.h ] || [ ! -f /usr/lib/libz.so ]) && ([ ! -f /usr/local/include/zlib.h ] || [ ! -f /usr/local/lib/libz.so ]); then
-		[ -d http://zlib.net/zlib-1.2.8.tar.gz ] || curl 'http://zlib.net/zlib-1.2.8.tar.gz' | sudo tar xz -C /usr/local/src
-		echo "Building zlib"
-		cd /usr/local/src/zlib-1.2.8
-		sudo ./configure
-		sudo make -j $CORE_COUNT install
-	else
-		echo "zlib is already installed."
-	fi
-
-	if ([ ! -f /usr/include/jpeglib.h ] || [ ! -f /usr/lib/libjpeg.so ]) && ([ ! -f /usr/local/include/jpeglib.h ] || [ ! -f /usr/local/lib/libjpeg.so ]); then
-		[ -d /usr/local/src/jpeg9b ] || curl 'http://www.ijg.org/files/jpegsrc.v9b.tar.gz' | sudo tar xz -C /usr/local/src
-		echo "Building libjpeg"
-		cd /usr/local/src/jpeg-9b
-		sudo ./configure
-		sudo make -j $CORE_COUNT install
-	else
-		echo "libjpeg is already installed."
-	fi
-
-	if ([ ! -f /usr/include/png.h ] || [ ! -f /usr/lib/libpng.so ]) && ([ ! -f /usr/local/include/png.h ] || [ ! -f /usr/local/lib/libpng.so ]); then
-		[ -d /usr/local/src/libpng-1.6.25 ] || curl -L 'http://download.sourceforge.net/libpng/libpng-1.6.25.tar.gz' | sudo tar xz -C /usr/local/src
-		echo "Building libpng"
-		cd /usr/local/src/libpng-1.6.25
-		sudo ./configure
-		sudo make -j $CORE_COUNT install
-	else
-		echo "libpng is already installed."
-	fi
-fi
-
-
-# Check for programs
-MISSING_PROGRAMS=0
-for i in bzip2 curl diff gcc git gzip make makeinfo patch pkg-config python svn tar wget; do
-	program_exists "$i" || { echo "Please install $i."; MISSING_PROGRAMS=1; }
-done
-
-[ $MISSING_PROGRAMS -eq 1 ] && exit 1
+sudo apt-get -y install binutils bzip2 curl gcc pkg-config libpng-dev libjpeg-dev diffutils git make subversion patch python tar texinfo wget
 
 # Create install directory
 INSTALL_DIR="/opt/toolchains/dc"
@@ -107,42 +8,153 @@ KOS="$INSTALL_DIR/kos"
 PORTS="$INSTALL_DIR/kos-ports"
 echo "Creating directory $INSTALL_DIR as root, giving users access to it."
 
-if program_exists sudo; then
-	sudo mkdir -p    "$INSTALL_DIR"
-	sudo chmod o+rwx "$INSTALL_DIR"
-else
-	mkdir -p "$INSTALL_DIR"
-fi
+sudo mkdir -p    "$INSTALL_DIR"
+sudo chmod o+rwx "$INSTALL_DIR"
+
 
 # Download the code for KOS + KOS-ports
 echo "Downloading KOS and KOS ports."
 [ -d "$KOS"   ] || git clone              git://git.code.sf.net/p/cadcdev/kallistios "$KOS"
 [ -d "$PORTS" ] || git clone  --recursive git://git.code.sf.net/p/cadcdev/kos-ports  "$PORTS"
 
+
 # Build the compiler
 # You need to change the Makefile if you want to install somewhere else.
-if ! program_exists "$INSTALL_DIR/sh-elf/bin/sh-elf-gcc" || ! program_exists "$INSTALL_DIR/sh-elf/bin/sh-elf-g++"; then
-	echo "Building the compiler."
-	cd   "$KOS/utils/dc-chain"
 
-	echo "Downloading.."; sh download.sh
-	echo "Unpacking..";   sh unpack.sh
+echo "Building the compiler."
+cd   "$KOS/utils/dc-chain"
 
-	# Update config.guess and config.sub to recognize msys
-	[ -s config.guess ] || wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
-	[ -s config.sub ]   || wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'   -O config.sub
-	cp config.guess config.sub binutils-*/
-	cp config.guess config.sub gcc-*/
-	cp config.guess config.sub newlib-*/
-	cp config.guess config.sub gcc-*/gmp
-	cp config.guess config.sub gcc-*/mpc
-	cp config.guess config.sub gcc-*/mpfr
+echo "Downloading.."
+# Getting configuration from Makefile
+source ./scripts/common.sh
 
-	sed -e "s/,objc,obj-c++//" -i.bak Makefile
-	
-	echo "Patching..";  make patch
-	echo "Compiling.."; make build
+print_banner "Downloader"
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    case $PARAM in
+        --config-guess-only)
+            CONFIG_GUESS_ONLY=1
+            ;;
+        *)
+            echo "error: unknown parameter \"$PARAM\""
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+function download()
+{
+  local name=$1
+  local ver=$2
+  local url=$3
+  local filename=$(basename $url)
+
+  if [ -n "$ver" ]; then
+    if [ ! -f $filename ]; then
+      echo "Downloading ${name} ${ver}..."
+      ${WEB_DOWNLOADER} "${DOWNLOAD_PROTOCOL}${url}" || exit 1
+    else
+      echo "$name $ver was already downloaded"
+	fi
+  fi
+}
+
+function download_dependencies()
+{
+  local arch=$1
+
+  local gmp_ver=$SH_GMP_VER
+  local mpfr_ver=$SH_MPFR_VER
+  local mpc_ver=$SH_MPC_VER
+  local isl_ver=$SH_ISL_VER
+  local gmp_url=$SH_GMP_URL
+  local mpfr_url=$SH_MPFR_URL
+  local mpc_url=$SH_MPC_URL
+  local isl_url=$SH_ISL_URL
+
+  if [ "$arch" == "arm" ]; then
+    gmp_ver=$ARM_GMP_VER
+    mpfr_ver=$ARM_MPFR_VER
+    mpc_ver=$ARM_MPC_VER
+    isl_ver=$ARM_ISL_VER
+    gmp_url=$ARM_GMP_URL
+    mpfr_url=$ARM_MPFR_URL
+    mpc_url=$ARM_MPC_URL
+    isl_url=$ARM_ISL_URL
+  fi
+
+  if [ "$USE_CUSTOM_DEPENDENCIES" == "1" ]; then
+    download "GMP"  "$gmp_ver"   "$gmp_url"
+    download "MPFR" "$mpfr_ver"  "$mpfr_url"
+    download "MPC"  "$mpc_ver"   "$mpc_url"
+    download "ISL"  "$isl_ver"   "$isl_url"
+  fi
+}
+
+# Download everything.
+if [ -z "${CONFIG_GUESS_ONLY}" ]; then
+  # Downloading SH components
+  download "Binutils" "$SH_BINUTILS_VER" "$SH_BINUTILS_URL"
+  download "GCC" "$SH_GCC_VER" "$SH_GCC_URL"
+  download_dependencies "sh"
+  download "Newlib" "$NEWLIB_VER" "$NEWLIB_URL"
+
+  # Downloading ARM components
+  download "Binutils" "$ARM_BINUTILS_VER" "$ARM_BINUTILS_URL"
+  download "GCC" "$ARM_GCC_VER" "$ARM_GCC_URL"
+  download_dependencies "arm"
 fi
+
+# Downloading config.guess.
+if [ ! -f ${CONFIG_GUESS} ]; then
+  WEB_DOWNLOAD_OUTPUT_SWITCH="-O"
+  if [ ! -z "${IS_CURL}" ] && [ "${IS_CURL}" != "0" ]; then
+    WEB_DOWNLOADER="$(echo ${WEB_DOWNLOADER} | cut -c-9)"
+    WEB_DOWNLOAD_OUTPUT_SWITCH="-o"
+  fi
+
+  echo "Downloading ${CONFIG_GUESS}..."
+  ${WEB_DOWNLOADER} ${WEB_DOWNLOAD_OUTPUT_SWITCH} ${CONFIG_GUESS} "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=${CONFIG_GUESS};hb=HEAD" || exit 1
+
+  # This is needed for all systems except MinGW.
+  chmod +x "./${CONFIG_GUESS}"
+fi
+
+echo "Done!"
+
+
+echo "Unpacking.."
+
+# Getting configuration from Makefile
+source ./scripts/common.sh
+
+print_banner "Unpacker"
+
+function unpack()
+{
+  local name="$1"
+  local ver="$2"
+  local ext="$3"
+
+  local dirname=$(tolower $name)-$ver
+  local filename=$dirnam
+
+# Update config.guess and config.sub to recognize msys
+[ -s config.guess ] || wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -O config.guess
+[ -s config.sub ]   || wget 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'   -O config.sub
+cp config.guess config.sub binutils-*/
+cp config.guess config.sub gcc-*/
+cp config.guess config.sub newlib-*/
+cp config.guess config.sub gcc-*/gmp
+cp config.guess config.sub gcc-*/mpc
+cp config.guess config.sub gcc-*/mpfr
+
+sed -e "s/,objc,obj-c++//" -i.bak Makefile
+
+echo "Patching..";  make patch
+echo "Compiling.."; make build
 
 # Create environment
 # You need to change environ.sh and environ_base.sh if you want to install somewhere else.
@@ -159,15 +171,6 @@ echo "Compiling KOS ports."
 sh "$PORTS/utils/build-all.sh"
 
 # Examples
-DOCURL='http://gamedev.allusion.net/docs/kos-current/structcont__state__t.html'
-if program_exists x-www-browser; then
-	x-www-browser "$DOCURL"
-elif program_exists firefox; then
-	firefox       "$DOCURL"
-elif program_exists chromium; then
-	chromium      "$DOCURL"
-fi
-
 echo ""
 echo "Welcome to KOS!"
 echo "Please check out the example in \`$KOS/examples/dreamcast/kgl/nehe/nehe02' to get started."
